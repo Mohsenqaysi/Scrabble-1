a75c1ca06f4a69a972f6cdbb4fc921387c99ccd1
Mohsen Qaysi <MohsenQaysi@Mohsens-MacBook-Pro.local>
2015-02-22 22:11:02 +0000

    Near Done playable version

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -1,28 +1,53 @@
 import java.util.Scanner;
 
-public class BoardTest
-{
-	public static void main(String[] args)
-	{
-		
-		Board testBoard = new Board();
-
+public class BoardTest {
+	public static void main(String[] args) {
+		String postion = "", direction = "";
+		boolean empty;
+		Frame frame;
 		Pool testPool = new Pool();
 		Frame testFrame = new Frame();
-		Scanner testInput = new Scanner(System.in);
-
+		
+		System.out.println("Pool of size: "+testPool.size());
+		empty = testFrame.isEmpty();
+		System.out.printf("Is frame empty? %b \n",empty);  // expect:true
+		Board testBoard = new Board();
 		System.out.println(testBoard);
 		// Test toString() method. Expect a blank board.
-		while ( !testPool.isEmpty() )
-		{
-			testFrame.refill(testPool);
-			System.out.println("Frame: " + testFrame);
-			System.out.println("<Starting Position> <Direction> <Word>");
-			testBoard.putNewWord(testInput.next(), testInput.next(), testInput.next(), testFrame);
-			// We do NOT do any input checking. Be careful with your input.
-			System.out.println(testBoard);
-		}
 		
+		Scanner testInput = new Scanner(System.in);
+
+		testFrame.refill(testPool);
+		System.out.println(testFrame);
+		// It has to start at position H8 which is the center.
+		testBoard.putNewWord("H8", "A", testInput.next(), testFrame);
+		testFrame.refill(testPool);
+		System.out.println(testBoard);
+		System.out.println(testFrame);
+
+		// Change the position and direction from the keyboard input.
+		System.out.printf("Give the postion in this formate H8 (is center): ");
+		postion = testInput.next();
+		System.out.printf("Give the direction\nA - horizontally\tD - vertically\n");
+		direction = testInput.next();
+		testBoard.putNewWord(postion.toUpperCase(), direction, testInput.next(), testFrame);
+		System.out.println("Pool of size: "+testPool.size()); // depends of the taken letters.
+		empty = testFrame.isEmpty();
+		System.out.printf("Is frame empty? %b \n",empty);  // expect:false
+		testFrame.refill(testPool);
+		System.out.println(testBoard);
+		System.out.println(testFrame);
+
+		// Change the position and direction from the keyboard input.
+		System.out.printf("Give the postion in this formate H8 (is center): ");
+		postion = testInput.next();
+		System.out.printf("Give the direction\nA - horizontally\tD - vertically\n");
+		direction = testInput.next();
+		testBoard.putNewWord(postion.toUpperCase(), direction, testInput.next(), testFrame);
+		System.out.println(testBoard);
+		System.out.println(testFrame);
+
 		testInput.close();
 	}
+
 }
